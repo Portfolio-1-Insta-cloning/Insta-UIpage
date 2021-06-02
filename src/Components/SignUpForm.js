7e62d912796cf6b89cfc1220a032351cbeb30b88
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-06-02 15:49:24 -0400

    signup form styling is done with CSS

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -2,104 +2,8 @@ import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
 import axios from "axios";
 import * as yup from "yup";
+import "../css/signup.css";
 import styled from 'styled-components';
-
-const SignUpWrapperDiv = styled.div`
-    margin-top: 0;
-    width: 100%;
-    background-color:  #3399ff;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    color: #ffffff;
-    height: auto;
-`;
-
-const FormHeaderDiv = styled.div`
-    width: 60%;
-    max-width: 500px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: baseline;
-`;
-
-const SignUpTitle = styled.h1`
-    margin-right: auto;
-    font-family: Source Sans Pro, sans-serif;
-    font-size: 3rem;
-    color: #ffffff;
-    font-weight: 600;
-    line-height: 1.65;
-    margin-bottom: 20px;
-`;
-
-const Form = styled.form`
-    width: 60%;
-    max-width: 500px;
-    margin: 25px auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid white;
-    padding: 25px;
-    padding-top: 50px;
-    padding-bottom: 50px;
-`;
-
-const FormaGroupWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-`;
-
-const ErrorMsg = styled.span`
-    font-family: Source Sans Pro, sans-serif;
-    color: #ff0000;
-    font-size: 1.5rem;
-    margin-bottom: 10px;
-`;
-
-const FormGroup = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items:center;
-`;
-
-const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-`;
-
-const Label = styled.label`
-    margin-right: auto;
-    margin-bottom: 10px;
-    font-size: 1.8rem;
-    font-weight: 600;
-    font-family: Source Sans Pro, sans-serif;
-`;
-
-const Required = styled.span`
-    color: #ff3300;
-    font-weight: 500;
-    margin-left: 5px;
-`;
-
-const Input = styled.input`
-    margin: 0;
-    height: auto;
-    border: none;
-    border-radius: 2px;
-    padding-left: 8px;
-    padding-top: 6px;
-    padding-bottom: 6px;
-    font-size: 1.8rem;
-    margin-bottom: 16px;
-    margin-left: 10px;
-    background-color: #ffffff;
-`;
 
 const Errors = styled.p`
     color: #9bf1ff;
@@ -107,26 +11,6 @@ const Errors = styled.p`
     font-size: 1.6rem;
     text-align: center;
 `;
-
-const SubmitButton = styled.button`
-    border: 1px solid #ffffff;
-    background-color: transparent;
-    padding: 8px 30px;
-    text-transformation: uppercase;
-    font-size: 1.8rem;
-    color: #ffffff;
-    border-radius: 2px;
-    margin-top: 10px;
-    &:hover{
-        border-color: #9bf1ff;
-        color: #9bf1ff;
-    }
-    &:disabled{
-        opacity: 0.5;
-        cursor: not-allowed;
-    }
-`;
-
 
 const SignUpForm = (props) => {
     
@@ -206,99 +90,99 @@ const SignUpForm = (props) => {
 
     return (
         <>
-        <SignUpWrapperDiv>
-            <FormHeaderDiv>
-                <SignUpTitle>Create an Account</SignUpTitle>
-            </FormHeaderDiv>
-                <Form onSubmit={submitHandler}>
-                <ErrorMsg>{uniqeUserName}</ErrorMsg>
-                <FormaGroupWrapper>
-                <FormGroup>
-                    <Label htmlFor="firstname">First Name
-                    <Required>*</Required></Label>
-                    <Input type="text"
-                        name="firstname"
-                        id="firstname"
-                        value = {signupForm.firstname}
-                        placeholder = "First Name"
-                        required
-                        onChange={inputchange} />
-                        {/* {errorState.firstname.length > 0 ? <Errors> {errorState.firstname }</Errors> : null } */}
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="lastname">Last Name
-                    <Required>*</Required></Label>
-                    <Input type="text"
+        <div className = "signup_wrapperdiv">
+            <div className = "form_header_div">
+                <h1 className = "signup_title">Create an Account</h1>
+            </div>
+                <form className = "form_ele" onSubmit={submitHandler}>
+                <span className = "error_msg">{uniqeUserName}</span>
+                <div className = "formGroup_wrapper">
+                    <div className = "formGroup_div">
+                        <label className = "label_ele" htmlFor="firstname">First Name
+                        <span className = "required">*</span></label>
+                        <input className = "input_ele" type="text"
+                            name="firstname"
+                            id="firstname"
+                            value = {signupForm.firstname}
+                            placeholder = "First Name"
+                            required
+                            onChange={inputchange} />
+                            {/* {errorState.firstname.length > 0 ? <p className = "validation_error"> {errorState.firstname }</p> : null } */}
+                    </div>
+                <div className = "formGroup_div">
+                    <label className = "label_ele" htmlFor="lastname">Last Name
+                    <span className = "required">*</span></label>
+                    <input className = "input_ele" type="text"
                         name="lastname"
                         id="lastname"
                         value={signupForm.lastname}
                         placeholder = "Last Name"
                         required
                         onChange={inputchange} />
-                        {/* {errorState.lastname.length > 0 ? <p> {errorState.lastname }<p> : null } */}
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="email">E-mail
-                    <Required>*</Required></Label>
-                    <Input type="email"
+                        {/* {errorState.lastname.length > 0 ? <p className = "validation_error"> {errorState.lastname }</p> : null } */}
+                </div>
+                <div className = "formGroup_div">
+                    <label className = "label_ele" htmlFor="email">E-mail
+                    <span className = "required">*</span></label>
+                    <input className = "input_ele" type="email"
                         name="email"
                         id="email"
                         value={signupForm.email}
                         placeholder = "username@domain.com"        
                         required
                         onChange={inputchange} />
-                        {/* {errorState.email.length > 0 ? <p> {errorState.email}<p> : null } */}    
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="phone">Phone Number
-                    <Required>*</Required></Label>
-                    <Input type="tel"
+                        {/* {errorState.email.length > 0 ? <p className = "validation_error"> {errorState.email}</p> : null } */}    
+                </div>
+                <div className = "formGroup_div">
+                    <label className = "label_ele" htmlFor="phone">Phone Number
+                    <span className = "required">*</span></label>
+                    <input className = "input_ele" type="tel"
                         name="phone"
                         id="phone"
                         value={signupForm.phonenumber}
                         placeholder = "123-456-7890"        
                         required
                         onChange={inputchange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="username">Username
-                    <Required>*</Required></Label>
-                    <Input type="text"
+                </div>
+                <div className = "formGroup_div">
+                    <label className = "label_ele" htmlFor="username">Username
+                    <span className = "required">*</span></label>
+                    <input className = "input_ele" type="text"
                         name="username"
                         id="username"
                         value={signupForm.username}
                         placeholder = "username"        
                         required
                         onChange={inputchange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="password">Password
-                    <Required>*</Required></Label>
-                    <Input type="password"
+                </div>
+                <div className = "formGroup_div">
+                    <label className = "label_ele" htmlFor="password">Password
+                    <span className = "required">*</span></label>
+                    <input className = "input_ele" type="password"
                         name="password"
                         id="password"
                         value={signupForm.password}
                         placeholder = "Password"    
                         required
                         onChange={inputchange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="confirmpassword">Confirm Password
-                    <Required>*</Required></Label>
-                    <Input type="password"
+                </div>
+                <div className = "formGroup_div">
+                    <label className = "label_ele" htmlFor="confirmpassword">Confirm Password
+                    <span className = "required">*</span></label>
+                    <input className = "input_ele" type="password"
                         name="confirmpassword"
                         id="confirmpassword"
                         value={signupForm.confirmpassword}
                         placeholder = "Confirm Password"
                         required
                         onChange={inputchange} />
-                </FormGroup>
-                </FormaGroupWrapper>
-                <ButtonWrapper>
-                    <SubmitButton type="submit">Signup</SubmitButton>
-                </ButtonWrapper>
-            </Form>
-            </SignUpWrapperDiv>
+                </div>
+                </div>
+                <div className = "button_wrapper">
+                    <button className = "submitbutton" type="submit">Signup</button>
+                </div>
+            </form>
+            </div>
         </>
     )
 }
